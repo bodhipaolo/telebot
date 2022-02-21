ab4a23b2ed0246c16a6c924a1e06729272e7a4da
Bodhipaolo <bodhipaolo@myzone.io>
2022-02-21 21:10:12 +0100

    inserted code for managing users and chats

--- telebot/internal_commands.py
+++ telebot/internal_commands.py
@@ -43,27 +43,6 @@ def start_command(update: Update, context: CallbackContext) -> None:
     logger.info(f"{username} started the chat {chat.id}")
 
 
-def start_command2(update: Update, context: CallbackContext) -> None:
-        """Bot presentation and chat initialization"""
-        # Let's check who is responsible for the change
-        username = update.effective_user.username
-
-        # Handle chat types differently:
-        chat = update.effective_chat
-        
-        if chat.type == Chat.PRIVATE:
-            logger.info(f"Add {username} user with {chat.id} chat id of individual chat")
-            context.bot_data.setdefault("user_ids", dict())
-            context.bot_data["user_ids"][username] = chat.id
-        elif chat.type in [Chat.GROUP, Chat.SUPERGROUP]:
-            logger.info(f"Add {username} user with {chat.id} chat id of {chat.title} group")
-            context.bot_data.setdefault("group_ids", dict())
-            context.bot_data["group_ids"][username] = chat.id
-        else:
-            logger.info(f"Add {username} user with {chat.id} chat id of {chat.title} channel")
-            context.bot_data.setdefault("channel_ids", dict())
-            context.bot_data["channel_ids"][username] = chat.id
-
 
 
 
